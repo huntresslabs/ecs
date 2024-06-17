b456b5337fd663482b68ed3bcd65629ff93b58cf
Luke Snyder <709836+lksnyder0@users.noreply.github.com>
2024-06-17 13:09:40 -0400

    Actually, don't need to be all the complicated

--- scripts/schema/loader.py
+++ scripts/schema/loader.py
@@ -184,7 +184,7 @@ def deep_nesting_representation(
 
         # Schema-only details. Not present on other nested field groups.
         schema_details: SchemaDetails = {}
-        for schema_key in ["root", "group", "reusable", "title", "settings", "type"]:
+        for schema_key in ["root", "group", "reusable", "title", "settings"]:
             if schema_key in flat_schema:
                 schema_details[schema_key] = flat_schema.pop(schema_key)
 
